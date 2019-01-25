8cd98351a05db42c00b09cdf70c72f5e6f436e56
Davoud Ataee Tarzanagh <tarzanagh@gmail.com>
2019-01-24 23:24:47 -0500

    Update local_degree.m

--- DADAM-master/dadam_networks/local_degree.m
+++ DADAM-master/dadam_networks/local_degree.m
@@ -1,4 +1,10 @@
 function W_FDLA=local_degree(P,L,eps_deg)
+
+% References: 
+%  W. Shi, Q. Ling, G. Wu, and W. Yin, “EXTRA: An Exact First-Order Algorithm for Decentralized Consensus Optimization,”
+%       SIAM Journal on Optimization, vol. 25, no. 2, pp. 944–966, 2015.
+%  S. Boyd, P. Diaconis, and L. Xiao, “Fastest mixing markov chain on a graph,” SIAM
+% review, vol. 46, no. 4, pp. 667–689, 2004.
 W_FDLA(1:L,1:L)=0;
 deg=sum(P,2);
 for i=1:L
